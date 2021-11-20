f9d9a6fdef50aa4e0ea76bc21548b2f0ee9a3fac
AbrahamJuarez <ajuarezdelacruz93@gmail.com>
2021-11-20 14:29:27 -0600

    Inicializando el proyecto. MS Cursos.

---
+++ src/main/java/com/sux/cursos/ms/app/MsSpringcloudAngularfullstackCursosApplication.java
@@ -0,0 +1,13 @@
+package com.sux.cursos.ms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringcloudAngularfullstackCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringcloudAngularfullstackCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sux/cursos/ms/app/MsSpringcloudAngularfullstackCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sux.cursos.ms.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringcloudAngularfullstackCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
